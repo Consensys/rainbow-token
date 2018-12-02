35eb2d0fab66855a30531e0a614e11e317db0385
Loic Vincent-Genod <loic.vincent-genod@consensys.net>
2018-12-02 16:58:28 +0100

    remove console.log

--- src/web3/formatters/transactionToEmitter.js
+++ src/web3/formatters/transactionToEmitter.js
@@ -4,11 +4,9 @@ export default (transaction) => {
     return eventChannel(emitter => {
         transaction
             .on("transactionHash", txHash => {
-                console.log("Transaction hash: ", txHash);
                 emitter({ type: "TRANSACTION_HASH", payload: txHash });
             })
             .on("receipt", receipt => {
-                console.log("Receipt: ", receipt);
                 emitter({ type: "RECEIPT", payload: receipt });
             })
             .on("error", error => {
