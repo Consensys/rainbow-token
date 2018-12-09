c1c1f611b0ab89561e47bad41b544b73c7a58ad1
Loic Vincent-Genod <loic.vincent-genod@consensys.net>
2018-12-09 20:45:33 +0100

    test

--- src/redux/sagas/transactions/rainbowToken.js
+++ src/redux/sagas/transactions/rainbowToken.js
@@ -1,122 +1,116 @@
-import { call, put, select, takeLatest, all } from 'redux-saga/effects';
+import { call, put, select, takeLatest, all } from "redux-saga/effects";
 
 import {
     START_PLAYING,
     REQUEST_BLEND,
-    SET_BLENDING_PRICE,
-} from '../../actions/transactions/rainbowToken';
+    SET_BLENDING_PRICE
+} from "../../actions/transactions/rainbowToken";
 
-import {
-    addError,
-} from '../../actions/errors';
+import { addError } from "../../actions/errors";
 
-import transactionHandler from './transactionHandler';
+import transactionHandler from "./transactionHandler";
 
-import { defaultBlendingPrice } from '../../../constants/rainbowToken';
+import { defaultBlendingPrice } from "../../../constants/rainbowToken";
 
 /** ******* WORKERS *********/
 
-function *startPlayingSaga () {
-  try {
-    const { address } = yield select(state => state.web3.account);
-    const { play } = yield select(
-      state => state.web3.contracts.RainbowToken.transactions
-    );
-    const chan = yield call(
-      play,
-      {
-        from: address,
-        value: defaultBlendingPrice
-      }
-    );
-    yield call(transactionHandler, chan);
-  } catch(err) {
-    yield put(addError('An error occured, please refresh the page.'));
-  }
+function* startPlayingSaga() {
+    try {
+        const { address } = yield select(state => state.web3.account);
+        const { play } = yield select(
+            state => state.web3.contracts.RainbowToken.transactions
+        );
+        const chan = yield call(play, {
+            from: address,
+            value: defaultBlendingPrice
+        });
+        yield call(transactionHandler, chan);
+    } catch (err) {
+        yield put(addError("An error occured, please refresh the page."));
+    }
 }
 
-function *blendSaga (blendingAddress, blendingToken) {
-  try {
-    const { address } = yield select(state => state.web3.account);
-    const { blend } = yield select(
-      state => state.web3.contracts.RainbowToken.transactions
-    );
-    const chan = yield call(
-      blend,
-      blendingAddress,
-      blendingToken.blendingPrice,
-      blendingToken.color.r,
-      blendingToken.color.g,
-      blendingToken.color.b,
-      {
-        from: address,
-        value: blendingToken.blendingPrice
-      });
-    yield transactionHandler(chan);
-  } catch(err) {
-    yield put(addError('An error occured, please refresh the page.'));
-  }
+function* blendSaga(blendingAddress, blendingToken) {
+    try {
+        const { address } = yield select(state => state.web3.account);
+        const { blend } = yield select(
+            state => state.web3.contracts.RainbowToken.transactions
+        );
+        const chan = yield call(
+            blend,
+            blendingAddress,
+            blendingToken.blendingPrice,
+            blendingToken.color.r,
+            blendingToken.color.g,
+            blendingToken.color.b,
+            {
+                from: address,
+                value: blendingToken.blendingPrice
+            }
+        );
+        yield transactionHandler(chan);
+    } catch (err) {
+        yield put(addError("An error occured, please refresh the page."));
+    }
 }
 
-function *defaultBlendSaga () {
-  try {
-    const { address } = yield select(state => state.web3.account);
-    const { defaultBlend } = yield select(
-      state => state.web3.contracts.RainbowToken.transactions
-    );
-    const chan = yield call(
-      defaultBlend,
-      {
-        from: address,
-        value: defaultBlendingPrice
-      });
-    yield transactionHandler(chan);
-  } catch(err) {
-    yield put(addError('An error occured, please refresh the page.'));
-  }
+function* defaultBlendSaga() {
+    try {
+        const { address } = yield select(state => state.web3.account);
+        const { defaultBlend } = yield select(
+            state => state.web3.contracts.RainbowToken.transactions
+        );
+        const chan = yield call(defaultBlend, {
+            from: address,
+            value: defaultBlendingPrice
+        });
+        yield transactionHandler(chan);
+    } catch (err) {
+        yield put(addError("An error occured, please refresh the page."));
+    }
 }
 
-function *setBlendingPriceSaga (price) {
-  try {
-    const { address } = yield select(state => state.web3.account);
-    const { setBlendingPrice } = yield select(
-      state => state.web3.contracts.RainbowToken.transactions
-    );
-    const chan = yield call(setBlendingPrice, price, { from: address });
-    yield transactionHandler(chan);
-  } catch(err) {
-    yield put(addError('An error occured, please refresh the page.'));
-  }
+function* setBlendingPriceSaga(price) {
+    try {
+        const { address } = yield select(state => state.web3.account);
+        const { setBlendingPrice } = yield select(
+            state => state.web3.contracts.RainbowToken.transactions
+        );
+        const chan = yield call(setBlendingPrice, price, { from: address });
+        yield transactionHandler(chan);
+    } catch (err) {
+        yield put(addError("An error occured, please refresh the page."));
+    }
 }
 
 /** ******* WATCHERS *********/
 
-function *watchStartPlaying () {
+function* watchStartPlaying() {
     yield takeLatest(START_PLAYING, startPlayingSaga);
 }
 
-function *watchRequestBlend () {
+function* watchRequestBlend() {
     yield takeLatest(
-      REQUEST_BLEND,
-      ({ payload: { blendingAddress, blendingToken } }) => {
-        if (blendingAddress && blendingToken) {
-          return blendSaga(blendingAddress, blendingToken);
-        } else {
-          return defaultBlendSaga();
+        REQUEST_BLEND,
+        ({ payload: { blendingAddress, blendingToken } }) => {
+            if (blendingAddress && blendingToken) {
+                return blendSaga(blendingAddress, blendingToken);
+            } else {
+                return defaultBlendSaga();
+            }
         }
-      }
     );
 }
 
-function *watchSetBlendingPrice () {
-    yield takeLatest(
-        SET_BLENDING_PRICE,
-        ({ payload }) => setBlendingPriceSaga(payload));
+function* watchSetBlendingPrice() {
+    yield takeLatest(SET_BLENDING_PRICE, ({ payload }) =>
+        setBlendingPriceSaga(payload)
+    );
 }
 
 /** ******* SAGA *********/
 
-export default function* () {
+export default function*() {
     yield all([
         watchStartPlaying(),
         watchRequestBlend(),
