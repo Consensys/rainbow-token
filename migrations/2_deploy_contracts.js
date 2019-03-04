f3574883296c2c846295d6d65ca0397c1101ccfe
Loïc Vincent-Genod <loic.vincent-genod@consensys.net>
2019-03-04 18:20:56 +0100

    change winning constant

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const RainbowToken = artifacts.require('./RainbowToken.sol');
 
 module.exports = function (deployer) {
-    deployer.deploy(RainbowToken, 44, 86, 221, 3);
+    deployer.deploy(RainbowToken, 44, 86, 221, 2);
 };
--- src/web3Config/index.js
+++ src/web3Config/index.js
@@ -10,7 +10,7 @@ export default {
       // Dev
       // address: '0xC61431eb6c69189F844A595234Ce331B7C8A7A77',
       // Ropsten
-      address: '0x61CB7a92eC398f01F4b70Dc71d68744bab7294f2'
+      address: '0xc0149Bee5AE61553506192ca8Eb876Ec7E6aeBFf'
       // address: '0x1266af25494414936AF6c9778EDDCE7ce3dedcd5'
       // address: '0x09e406e528fEa8190a091830e6935dBc12624AE2'
     },
